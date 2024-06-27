7c55df5ed10a681493def815a9730438d473636d
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-06-27 16:59:04 +0700

    Switch Branch to 1.20.1 Fabricated

--- src/main/java/ru/paulevs/bismuthlib/mixin/TextureAtlasSpriteAccessor.java
+++ src/main/java/ru/paulevs/bismuthlib/mixin/TextureAtlasSpriteAccessor.java
@@ -2,6 +2,7 @@ package ru.paulevs.bismuthlib.mixin;
 
 import com.mojang.blaze3d.platform.NativeImage;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
